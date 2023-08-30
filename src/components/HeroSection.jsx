d2181628351798c73a2ce0c867c9f146d4c1221a
Kevin <kevpadilla01@gmail.com>
2023-08-30 18:39:09 -0300

    fix: paragraph line height

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ function HeroSection () {
     <section className='flex flex-col-reverse md:flex md:flex-row md:justify-between md:items-center mx-auto max-w-sm md:max-w-full w-full h-[660px] md:h-[550px]'>
       <div className='flex flex-col items-center text-center space-y-6 md:text-left md:justify-center md:items-start md:space-y-10 md:w-[450px] md:h-full'>
         <h1 className='text-DarkBlue text-[42px] font-semibold leading-[1.1]'>Bring everyone together to build better products.</h1>
-        <p className='text-sm w-[280px] lg:w-[300px] text-DarkGrayishBlue'>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goats in view.
+        <p className='text-sm w-[280px] lg:w-[300px] text-DarkGrayishBlue leading-[1.6]'>Manage makes it simple for software teams to plan day-to-day tasks while keeping the larger team goats in view.
         </p>
         <div>
           <Button />
